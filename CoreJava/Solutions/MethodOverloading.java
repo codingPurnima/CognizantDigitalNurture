af4c1dcfb8f04398dc98fd8482633b4877314fa0
Purnima Pandey <purnimapnd04@gmail.com>
2026-05-25 10:31:38 +0530

    Understand method overloading

---
+++ CoreJava/Solutions/MethodOverloading.java
@@ -0,0 +1,22 @@
+class MethodOverloading {
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public static void main(String[] args) {
+        MethodOverloading obj = new MethodOverloading();
+
+        System.out.println(obj.add(2,3));
+        System.out.println(obj.add(2.5,3.5));
+        System.out.println(obj.add(1,2,3));
+    }
+}
