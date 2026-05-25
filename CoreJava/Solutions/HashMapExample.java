4b2a9a381de7d729e28f03b70cfae5fa4f82e3d5
Purnima Pandey <purnimapnd04@gmail.com>
2026-05-25 10:37:35 +0530

    Use key-value pairs

---
+++ CoreJava/Solutions/HashMapExample.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class HashMapExample {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        HashMap<Integer, String> map= new HashMap<>();
+        map.put(1, "Ajay");
+        map.put(2, "Bijay");
+        map.put(3, "Charlie");
+        map.put(4, "Chetan");
+        map.put(5, "Deepak");
+        System.out.println(map);
+    }
+}
