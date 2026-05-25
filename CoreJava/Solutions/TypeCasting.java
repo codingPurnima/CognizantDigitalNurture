99d9bca6c7a74d8af09e57ded94ce6ac43ae3449
Purnima Pandey <purnimapnd04@gmail.com>
2026-05-25 10:28:35 +0530

    Type casting between data types

---
+++ CoreJava/Solutions/TypeCasting.java
@@ -0,0 +1,10 @@
+public class TypeCasting {
+    public static void main(String[] args) {
+        double d= 14.23;
+        int cast= (int) d;
+        System.out.println("Double casted to int:"+ cast);
+        int i= 86;
+        double x= i;
+        System.out.println("Int casted to double:"+ x);
+    }
+}
