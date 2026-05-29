ffba539bdf2a4968a84753186a7b9e930073c690
Purnima Pandey <purnimapnd04@gmail.com>
2026-05-29 14:27:25 +0530

    Process collections using streams

---
+++ CoreJava/Solutions/Stream.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Stream {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1,2,3,4,5,6);
+
+        List<Integer> even =
+                nums.stream()
+                    .filter(n -> n % 2 == 0)
+                    .collect(Collectors.toList());
+
+        System.out.println(even);
+    }
+}
