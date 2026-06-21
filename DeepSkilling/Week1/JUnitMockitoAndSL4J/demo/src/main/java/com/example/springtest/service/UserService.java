cd73f77f948b926b64a8a00610b9e4001d366847
Purnima Pandey <purnimapnd04@gmail.com>
2026-06-21 19:55:19 +0530

    Spring Testing

---
+++ DeepSkilling/Week1/JUnitMockitoAndSL4J/demo/src/main/java/com/example/springtest/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.springtest.service;
+
+import com.example.springtest.model.User;
+import com.example.springtest.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("User not found"));
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
