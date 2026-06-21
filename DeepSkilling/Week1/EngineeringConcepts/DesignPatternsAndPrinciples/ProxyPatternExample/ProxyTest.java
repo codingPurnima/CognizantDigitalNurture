5a8521fd2f3d40e48e4a95d3900f0a998986d89b
Purnima Pandey <purnimapnd04@gmail.com>
2026-06-21 11:54:55 +0530

    Proxy Pattern Example

---
+++ DeepSkilling/Week1/EngineeringConcepts/DesignPatternsAndPrinciples/ProxyPatternExample/ProxyTest.java
@@ -0,0 +1,13 @@
+public class ProxyTest {
+
+    public static void main(String[] args) {
+
+        Image image = new ProxyImage("photo.jpg");
+
+        System.out.println("First Display:");
+        image.display();
+
+        System.out.println("\nSecond Display:");
+        image.display();
+    }
+}
